6eba96380e3c79a71cb2bf00154257849de2e6fb
Yazeed Asaad <159422722+YazeedAsaad01@users.noreply.github.com>
2024-02-12 20:58:38 -0800

    Update code.js
    added comments

--- code.js
+++ code.js
@@ -1,7 +1,12 @@
+// Function definition for 'converter'
 function converter() {
+    // Prompt the user to input a word or a sentence and store it in the 'info' variable
     const info = prompt('Give me a word or a sentence');
+    // Get the length of the input string and store it in the 'length' variable
     const length = info.length;
+    // Return the length of the input string
     return length;
 }
 
+// Call the 'converter' function to obtain the length of the input string, then log the result to the console
 console.log(converter());
